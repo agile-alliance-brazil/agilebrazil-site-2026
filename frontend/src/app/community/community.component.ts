9d0e03af7419649f76d90bce98bb981664e63072
José Geraldo da Costa <jgt.josegeraldo@gmail.com>
2026-06-12 07:00:29 -0300

    Acerto texto

--- frontend/src/app/community/community.component.ts
+++ frontend/src/app/community/community.component.ts
@@ -9,7 +9,7 @@ export class CommunityComponent {
   comunidades = [
     {
       name: '6 e Byte',
-      description: '<p > <strong>6 e Byte</strong> <br/> Somos profissionais da área de tecnologia. Nesse canal temos um único propósito: Falar sobre tecnologia de uma maneira leve e discontraída, trazendo dos mais variados profissionais da área.</p>',
+      description: '<p class="title"> <strong >6 e Byte</strong> <br/> Somos profissionais da área de tecnologia. Nesse canal temos um único propósito: Falar sobre tecnologia de uma maneira leve e discontraída, trazendo dos mais variados profissionais da área.</p>',
       imageUrl: '/2026/assets/images/6-e-Byte.jpg',
       tags: ['Produto'],
       instagram: 'https://www.instagram.com/6ebyte',
@@ -17,7 +17,7 @@ export class CommunityComponent {
     },
     {
       name: 'DEVPIRA',
-      description: '<p> <strong>DEVPIRA</strong> <br/> Somos uma comunidade feita de "pessoal do TI" para "pessoal do TI": programadores, POs, QAs, PMs, designers e apaixonados por pessoas e tecnologia. Sem fins lucrativos, disseminamos conhecimento em Piracicaba, onde “o peixe para”, mas a tecnologia avança e inspira novas pessoas.</p>',
+      description: '<p class="title"> <strong>DEVPIRA</strong> <br/> Somos uma comunidade feita de "pessoal do TI" para "pessoal do TI": programadores, POs, QAs, PMs, designers e apaixonados por pessoas e tecnologia. Sem fins lucrativos, disseminamos conhecimento em Piracicaba, onde “o peixe para”, mas a tecnologia avança e inspira novas pessoas.</p>',
       imageUrl: '/2026/assets/images/logo-devpira.png',
       tags: ['Agilidade', 'Produto', 'Tecnologia'],
       instagram: 'https://www.instagram.com/devpira/',
@@ -25,7 +25,7 @@ export class CommunityComponent {
     },
     {
       name: 'Agile Minas',
-      description: '<p> <strong>Agile Minas</strong> <br/> Comunidade de agilistas para agilistas, que estejam interessados em compartilhar experiências e aprender com outros membros da comunidade mineira!</p>',
+      description: '<p class="title"> <strong>Agile Minas</strong> <br/> Comunidade de agilistas para agilistas, que estejam interessados em compartilhar experiências e aprender com outros membros da comunidade mineira!</p>',
       imageUrl: '/2026/assets/images/agile-minas.png',
       tags: ['Agilidade'],
       linkedin: 'https://www.linkedin.com/company/agile-minas/posts/?feedView=all',

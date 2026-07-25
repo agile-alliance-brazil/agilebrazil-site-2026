3dc89a10652a89a2c300cc98c75dcc4545fb6b75
José Geraldo da Costa <jgt.josegeraldo@gmail.com>
2026-07-25 08:44:51 -0300

    Atualização questão do keynote

--- frontend/src/app/submission/submission.component.ts
+++ frontend/src/app/submission/submission.component.ts
@@ -9,14 +9,14 @@ import { timeout } from 'rxjs/internal/operators/timeout';
 export class SubmissionComponent {
 
   ngAfterViewInit() {
-    const loading = document.querySelector('.loading') as HTMLElement;
-    loading.style.display = 'block';
+    /*const loading = document.querySelector('.loading') as HTMLElement;
+    loading.style.display = 'block';*/
     var script = document.createElement('script');
     script.src = 'https://www.even3.com.br/widget/js?e=agilebrazil2026-746128&t=submission&lang=pt';
     script.async = true;
     document.body.appendChild(script);
-    setTimeout(() => {
+    /*setTimeout(() => {
       loading.style.display = 'none';
-    }, 1500);
+    }, 1500);*/
   }
 }

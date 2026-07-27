0299043488b711d5fee4b2608da0d33793c0035c
José Geraldo da Costa <jgt.josegeraldo@gmail.com>
2026-07-27 05:42:49 -0300

    Remoção parte de submissão de palestras

--- frontend/src/app/submission/submission.component.ts
+++ frontend/src/app/submission/submission.component.ts
@@ -10,12 +10,12 @@ export class SubmissionComponent {
 
   ngAfterViewInit() {
     /*const loading = document.querySelector('.loading') as HTMLElement;
-    loading.style.display = 'block';*/
+    loading.style.display = 'block';
     var script = document.createElement('script');
     script.src = 'https://www.even3.com.br/widget/js?e=agilebrazil2026-746128&t=submission&lang=pt';
     script.async = true;
     document.body.appendChild(script);
-    /*setTimeout(() => {
+    setTimeout(() => {
       loading.style.display = 'none';
     }, 1500);*/
   }
